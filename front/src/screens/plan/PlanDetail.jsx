3a191c9ee10f430d5ad0cb0cf239b7f72cb5977e
jmlee9707 <jmlee9707@naver.com>
2022-07-22 17:54:56 +0900

    feat : 계획하기 메인 페이지 퍼블리싱 #13

---
+++ front/src/screens/plan/PlanDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PlanDetail() {
+  return (
+    <div className="container">
+      <div className="plan">dd</div>
+    </div>
+  );
+}
+
+export default PlanDetail;
