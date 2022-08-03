d0279d4a19b6ee92ec1a6a550e6b968e6041b238
jmlee9707 <jmlee9707@naver.com>
2022-08-03 16:15:13 +0900

    feat : 모달창 달력 api 구현 #19

--- front/src/components/camping/AddPlanModal.jsx
+++ front/src/components/camping/AddPlanModal.jsx
@@ -1,13 +1,18 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import useOutSideClick from "../../utils/useOutSideClick";
+// 달력 api
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// import { ko } from "date-fns/esm/locale";
 import "./AddPlanModal.scss";
+import useOutSideClick from "../../utils/useOutSideClick";
 
 function Modal({ onClose }) {
   const navigate = useNavigate();
   const handleClose = () => {
     onClose?.();
   };
+  // const locale = { ko };
 
   // useEffect(() => {
   //   const $body = document.querySelector("body");
@@ -16,12 +21,17 @@ function Modal({ onClose }) {
   // }, []);
 
   const modalRef = useRef(null);
+  const tripRef = useRef(""); // 여행 제목 입력 값
 
   useOutSideClick(modalRef, handleClose); // ref 밖의 요소 선택하면 함수 실행
 
   const addPlan = () => {
     navigate("/plan/detail");
   };
+
+  // 현재 날짜값으로 초기화
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   return (
     <div className="overlay">
@@ -30,12 +40,45 @@ function Modal({ onClose }) {
         className="modal flex column align-center justify-center"
       >
         <div className="modal_body flex column align-center justify-center">
-          <div className="modal_body_txt">
+          <div className="modal_body_txt flex column align-center">
             <div className="modal_body_txt_title notoBold fs-32">
               수완동 캠핑장
             </div>
-            <div className="modal_body_txt_days notoMid fs-20">날짜 : </div>
-            <div className="modal_body_txt_trip notoMid fs-20">제목 : </div>
+            <div className="modal_body_txt_days flex align-center notoMid fs-24">
+              날짜 :
+              <div className="flex modal_body_txt_days_select flex ">
+                <DatePicker
+                  selected={startDate}
+                  onChange={date => setStartDate(date)}
+                  selectsStart
+                  startDate={startDate}
+                  endDate={endDate}
+                  // locale={ko}
+                  dateFormat="yyyy/MM/dd"
+                  className="calen notoMid fs-22"
+                />
+                ~
+                <DatePicker
+                  selected={endDate}
+                  onChange={date => setEndDate(date)}
+                  selectsEnd
+                  startDate={startDate}
+                  endDate={endDate}
+                  minDate={startDate}
+                  // locale={ko}
+                  dateFormat="yyyy/MM/dd"
+                  className="calen notoMid fs-22"
+                />
+              </div>
+            </div>
+            <div className="modal_body_txt_trip notoMid fs-24">
+              제목 :
+              <input
+                className="fs-24 notoMid modal_body_txt_trip_input"
+                ref={tripRef}
+                placeholder="어떤 여행인가요?"
+              />
+            </div>
           </div>
           <div className="modal_body_btn flex">
             <button
