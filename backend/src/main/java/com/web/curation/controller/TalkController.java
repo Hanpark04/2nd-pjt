0c57382489df0f16e5b9a8547c296d9805fb60fe
김수빈 <chfhrdl0001@naver.com>
2022-08-03 16:20:13 +0900

    fix:talk 수정 #23

--- backend/src/main/java/com/web/curation/controller/TalkController.java
+++ backend/src/main/java/com/web/curation/controller/TalkController.java
@@ -1,12 +1,10 @@
 package com.web.curation.controller;
 
-import com.web.curation.data.dto.PhotoDto;
 import com.web.curation.data.dto.TalkDto;
-import com.web.curation.service.PhotoService;
 import com.web.curation.service.TalkService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +22,7 @@ import java.util.UUID;
 @RestController
 //@CrossOrigin("*")
 @RequestMapping("/talk")
+@RequiredArgsConstructor
 
 public class TalkController {
     private final Logger LOGGER = LoggerFactory.getLogger(PhotoController.class);
@@ -31,11 +30,6 @@ public class TalkController {
     private static final String FAIL = "fail";
 
     private final TalkService talkService;
-
-    @Autowired
-    public TalkController(TalkService talkService) {
-        this.talkService = talkService;
-    }
 
     @GetMapping
     public ResponseEntity<List<TalkDto>> listTalk() {
