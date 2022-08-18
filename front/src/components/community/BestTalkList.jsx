562100f77b927baf7f8dd7b5df48cfbec98e5d59
han <qkrgks378@gmail.com>
2022-08-19 06:28:02 +0900

    feat : 토크 리스트 반응형

--- front/src/components/community/BestTalkList.jsx
+++ front/src/components/community/BestTalkList.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { v4 } from "uuid";
 import BestTalk from "./BestTalk";
-import "./BestTalk.scss";
+// import "./BestTalk.scss";
 import { getTalkBest } from "../../apis/talk";
 // import { Link } from "react-router-dom";
 
 function BestTalkList() {
   const [bestTalkList, setBestTalkList] = useState([]); // 불러온 데이터 저장
-  // 베스트포토api 호출, 8개 담김
+  // 베스트토크api 호출, 6개 담김
   useEffect(() => {
     // await 를 사용하기 위해서 Async 선언
     async function getAndSetBestTalk() {
@@ -21,7 +21,7 @@ function BestTalkList() {
   // const newBestPhotoList = BestPhotoList.slice(0[ 4]);
 
   return (
-    <div className="bestTalkList flex">
+    <div className="best_talk_list flex">
       {bestTalkList.length !== 0 &&
         bestTalkList.map(({ blobFile, talkId, nickname, content, title }) => (
           <BestTalk
